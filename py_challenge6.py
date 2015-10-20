f7da2c5045e821339b9460b84467741cdd0ddcc5
Maureen Slavin <mslavin@mslavin-mba.local>
2015-10-20 15:23:49 -0500

    adding challenge 6

---
+++ py_challenge6.py
@@ -0,0 +1,35 @@
+
+start = '/Users/mslavin/Downloads/channel/90052.txt'
+
+order = []
+
+
+def get_next(start):
+    with open(start, 'r') as f:
+        text = f.read()
+        for item in text.split():
+            if item.isdigit():
+                number = item
+                order.append(item)
+                start = '/Users/mslavin/Downloads/channel/%s.txt' % number
+                get_next(start)
+            elif not any(ch.isdigit() for ch in text):
+                print("STOPPED", text)
+                break
+
+
+def collect_comments():
+    import zipfile
+    new = []
+    zf = zipfile.ZipFile('/Users/mslavin/Downloads/channel.zip', 'r')
+    for item in order:
+        file = '%s.txt' % item
+        new.append(zf.getinfo(file).comment.decode("utf-8"))
+
+    print(' '.join(ch for ch in new))
+
+
+
+
+get_next(start)
+collect_comments()
